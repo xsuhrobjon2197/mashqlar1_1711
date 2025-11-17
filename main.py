6eede5a73fc5be18ac94d1b90eeae56d882a6dbc
xsuhrobjon2197 <xsuhrobjon2197@gmail.com>
2025-11-17 02:24:30 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,69 @@
+#1-misol
+s = "abcdefghij"
+print(s)
+
+new = s[2:5]
+print(new)
+
+#2-misol
+s = "Hello World"
+print(s)
+
+toza = s[5]
+print(toza)
+
+#3-misol
+s = "PythonProgramning"
+print(s)
+
+toza = s[-7:]
+print(toza)
+
+#4-misol
+s = "abcdefghij"
+print(s)
+
+toza = s[::-1]
+print(toza)
+
+#5-misol
+s = "0123456789"
+print(s)
+
+toza = s[::2]
+print(toza)
+
+#6-misol
+s = "WelcomeToPython"
+print(s)
+
+toza = s[3:9]
+print(toza)
+
+#7-misol
+s = "PythonLanguage"
+print(s)
+
+toza = s[3:10]
+print(toza)
+
+#8-misol
+s = "ABCDEFGHIJK"
+print(s)
+
+toza = s[5:10]
+print(toza)
+
+#9-misol
+s = "superprogramer"
+print(s)
+
+toza = s[-4:]
+print(toza)
+
+#10-misol
+s = '0123456789'
+print(s)
+
+toza = s[::2]
+# print(toza)
